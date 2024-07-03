288d9179c48cd9f1fd5e1f2874b9b3e22d469bb4
Mackaber <Mackaber@users.noreply.github.com>
2024-07-02 21:30:46 -0600

    map y filter

--- 06-react-state-events/src/App.jsx
+++ 06-react-state-events/src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   const [name, setName] = useState("");
   const [age, setAge] = useState(0);
 
+  const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   const inputChange = (event) => {
     setName(event.target.value)
   }
@@ -19,7 +21,25 @@ function App() {
       <label htmlFor="age">Age: </label>
       <input type="number" onChange={(event) => setAge(event.target.value)} />
 
-      <h3>Hola {name} tienes {age} años</h3>
+      <h3>
+        Hola {name} tienes {age} años
+      </h3>
+
+
+      <h3>Numeros multiplicados por 2</h3>
+      {numeros.map((n) => (
+        <p key={n}>
+          el numero {n} multiplicado por 2 es {n * 2}
+        </p>
+      ))}
+
+      <h3>Numeros pares</h3>
+      {numeros.filter((n) => n % 2 === 0)
+        .map((n) => (
+        <p key={n}>
+          el numero {n} es par
+        </p>
+      ))}
     </form>
   );
 }
