246daf389dacf4adf0715a5482c9692665f1e00d
DoloresPGz <dolores.pgz@gmail.com>
2024-07-16 19:49:57 -0600

    uso de rutas en pokebootstrap

--- 08-poke-bootstrap/src/App.jsx
+++ 08-poke-bootstrap/src/App.jsx
@@ -1,13 +1,45 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import About from './pages/About';
 import Home from './pages/Home'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import PokemonDetail from './pages/PokemonDetail';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar/>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/pokemon",
+        element: <PokemonDetail />,
+      }
+    ],
+  },
+ 
+]);
 
 function App() {
 
   return (
     <>
-      <Navbar/>
-      <Home/>
+      <RouterProvider router={router}>
+       
+      </RouterProvider>
+
     </>
   )
 }
--- 08-poke-bootstrap/src/components/Navbar.jsx
+++ 08-poke-bootstrap/src/components/Navbar.jsx
@@ -1,18 +1,26 @@
+import { NavLink, Outlet } from "react-router-dom"
+
+//NavLink lo utilizamos como sustituto de la etiqueta a, el cual no renderiza toda la pantalla. En lugar de href, utiliza un
+//atributo llamato to
 const Navbar = () => {
     return (
-        <nav className='navbar navbar-expand-sm navbar-light bg-warning d-flex justify-content-between px-4'>
-            <a className='navbar-brand' href='#'>Pokédex</a>
+        <>
+            <nav className='navbar navbar-expand-sm navbar-light bg-warning d-flex justify-content-between px-4'>
+            <NavLink className='navbar-brand' to='#'>Pokédex</NavLink>
 
             <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
                 <li className='nav-item px-2'>
-                    <a className='nav-link' href='/'>Home</a>
+                    <NavLink className='nav-link' to='/'>Home</NavLink>
                 </li>
                 <li className='nav-item px-2'>
-                    <a className='nav-link' href='/about'>Acerca de Pokémon</a>
+                    <NavLink className='nav-link' to='/about'>Acerca de Pokémon</NavLink>
                 </li>
             </ul>
 
-        </nav>
+            </nav>
+            <Outlet></Outlet>
+        </>
+        
 
 
     )
